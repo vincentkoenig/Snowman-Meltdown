9cb580d707f52bb5ea001731d0e477da395ee104
Vincent König <vincentkoenig97@gmail.com>
2026-03-27 18:21:43 +0100

    move replay loop to snowman.py as main entry point

--- snowman.py
+++ snowman.py
@@ -1,2 +1,10 @@
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
+from game_logic import play_game
+
+if __name__ == "__main__":
+    while True:
+        play_game()
+
+        again = input("Play again? (y/n): ").lower()
+        if again != "y":
+            print("Thanks for playing!")
+            break
